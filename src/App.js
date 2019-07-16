82f34179da0ca0f2d19cf32169060fae4e62d631
Patrick Peter Zohdy <zohdy@Patricks-MacBook-Pro.local>
2019-07-17 00:27:50 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import QuoteBox from "./QuoteBox";
 
-function App() {
+const quotes = [
+  {
+    author: "Buddha",
+    text: "All that we are is the result of what we have thought",
+    color: "#f44336"
+  },
+  {
+    author: "Albert Einstein",
+    text: "I have no special talent. I am only passionately curious.",
+    color: "#e91e63"
+  },
+  {
+    author: "Mother Teresa",
+    text: "If you judge people, you have no time to love them",
+    color: "#673ab7"
+  },
+  {
+    author: "William Shakespeare ",
+    text: "Wisely, and slow. They stumble that run fast",
+    color: "#3f51b5"
+  },
+  {
+    author: "Coco Chanel",
+    text: "The most courageous act is still to think for yourself. Aloud.",
+    color: "#2196f3"
+  },
+  {
+    author: "Steve Jobs",
+    text: "Stay hungry, stay foolish.",
+    color: "#03a9f4"
+  },
+  {
+    author: "Ralph Waldo Emerson",
+    text: "A great man is always willing to be little",
+    color: "#00bcd4"
+  }
+];
+
+const App = () => {
+  const randomIndex = () => Math.floor(Math.random() * quotes.length);
+  const [quote, setQuote] = useState(quotes[randomIndex()]);
+  const getRandomQuote = () => {
+    let currentIndex = randomIndex();
+    let currentQuote = quotes[currentIndex];
+
+    if (currentQuote !== quote) {
+      setQuote(currentQuote);
+    } else if (currentIndex === quotes.length - 1) {
+      setQuote(quotes[currentIndex - 1]);
+    } else {
+      setQuote(quotes[currentIndex + 1]);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ background: quote.color }}>
+      <QuoteBox getQuote={getRandomQuote} quote={quote} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/QuoteBox.js
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import "./QuoteBox.css";
+import Card from "@material-ui/core/Card";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    box: {
+      height: "100vh",
+      background: "#fff",
+      width: "50%",
+      height: "50%",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-between",
+      overflow: "scroll"
+    },
+    buttonWrapper: {
+      margin: theme.spacing(2),
+      display: "flex",
+      justifyContent: "center",
+      flexWrap: "wrap"
+    },
+    content: {
+      margin: theme.spacing(2),
+      color: (props) => props.color,
+      transition: "all 1s",
+      display: "flex",
+      flexDirection: "column"
+    },
+    blockquote: {
+      background: "#f9f9f9",
+      borderLeft: "10px solid #ccc",
+      fontStyle: "italic",
+      letterSpacing: "1px",
+      fontSize: "1.3em",
+      position: "relative",
+      lineHeight: 1.6,
+      padding: "20px 20px 20px 70px",
+      "&:before": {
+        transition: "all 1s",
+        fontFamily: "arial",
+        content: "“",
+        content: "open-quote",
+        color: (props) => props.color,
+        fontSize: "4em",
+        position: "absolute",
+        left: "10px",
+        top: "-10px"
+      }
+    },
+    author: {
+      marginLeft: "auto"
+    },
+    button: {
+      minWidth: "130px",
+      marginTop: "1rem",
+      marginLeft: "1rem",
+      marginRight: "1rem",
+      transition: "all 1s",
+      background: (props) => props.color,
+      color: "#fff",
+      "&:hover": {
+        background: (props) => props.color,
+        opacity: "0.8"
+      }
+    }
+  })
+);
+
+const QuoteBox = ({ getQuote, quote }) => {
+  const classes = useStyles(quote);
+  const { author, text } = quote;
+  return (
+    <Card id="quote-box" className={classes.box}>
+      <CardContent className={classes.content}>
+        <Typography className={classes.blockquote} id="text">
+          {text}
+        </Typography>
+        <Typography
+          className={classes.author}
+          id="author"
+        >{`- ${author}`}</Typography>
+      </CardContent>
+      <CardActions className={classes.buttonWrapper}>
+        <Button className={classes.button} size="large" variant="contained">
+          <a
+            id="tweet-quote"
+            href={`https://twitter.com/intent/tweet?text=${text},${author}`}
+          >
+            Tweet
+          </a>
+        </Button>
+        <Button
+          className={classes.button}
+          size="large"
+          variant="contained"
+          id="new-quote"
+          onClick={getQuote}
+        >
+          New Quote
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default QuoteBox;
